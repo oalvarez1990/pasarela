eea938e3890a5bcf35fded7644053c5403e3ddf3
Omar Aleiser Alvarez Laverde <omaraleyser@hotmail.com>
2022-12-05 12:26:31 -0500

    Crea frontend

---
+++ scritp/script.js
@@ -0,0 +1,17 @@
+(function () {
+  var updateButton = document.getElementById("updateDetails");
+  var cancelButton = document.getElementById("cancel");
+  var compra_modal = document.getElementById("compra_modal");
+
+  // Update button opens a modal dialog
+  updateButton.addEventListener("click", function () {
+    console.log('Bien')
+    compra_modal.showModal();
+    console.log('aquiiiiiiiiiiiii')
+  });
+
+  // Form cancel button closes the dialog box
+  cancelButton.addEventListener("click", function () {
+    compra_modal.close();
+  });
+})();
